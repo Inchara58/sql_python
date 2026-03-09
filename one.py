faf3f0db5f6a997403fadb934be67abba9b0e236
Inchara58 <incharadr58@gmail.com>
2026-03-09 18:57:47 +0530

    ppp

--- one.py
+++ one.py
@@ -5,3 +5,19 @@ print(new_user)
 print(len(username))
 print(username.upper())
 print("hi")
+
+patient = "   rAHUL sHARMA   "
+print(patient.strip())
+print(patient.title())
+
+data = "Alice,18,Bangalore,CSE,9.3"
+data_name="Alice"
+data_age=18
+data_place="Banglore"
+data_branch="CSE"
+data_sgpa=9.3
+print(data_name)
+print(data_age)
+print(data_branch)
+print(data_place)
+print(data_sgpa)
