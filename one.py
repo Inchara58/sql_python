718c85b4a839f026790fd7b2f6bb80361c176f46
Inchara58 <incharadr58@gmail.com>
2026-03-09 19:45:06 +0530

    prac

--- one.py
+++ one.py
@@ -21,3 +21,28 @@ print(data_age)
 print(data_branch)
 print(data_place)
 print(data_sgpa)
+
+date = "2024-01-15"
+print(date[0:4])
+print(date[5:7])
+print(date[8:10])
+
+email1 = "alice@gmail.com"
+email2 = "rahulgmail.com"
+email3 = "sneha@yahoo"
+
+if "@" in email1 and "." in email1:
+    print("email1 is Valid")
+else:
+    print("email1 is Invalid")
+
+if "@" in email2 and "." in email2 :
+    print("email2 is valid")
+else:
+    print("email2 is Invalid")
+
+if "@" in email3 and "." in email3 :
+    print("email3 is valid")
+else:
+    print("email3 is Invalid")
+
